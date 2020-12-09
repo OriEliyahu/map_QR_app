b876b307b32d4582b173da9418e5ce61c8336e04
Omatan10 <omertavor13@gmail.com>
2020-12-09 14:49:49 +0200

    FEATURE - Added the map from mapbox package. works good !

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+MapboxGL.setAccessToken('pk.eyJ1Ijoib21lcnRhdm9ybWFwIiwiYSI6ImNraWg5ZmZtbDBmYnkzM28zdjJzaW93MzcifQ.eo0ZXB8ZY1hQ3GMp6gJjbA');
 
 const App = () => {
-    return (
-        <View>
-            <Text>
-                Hello world !
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+         <MapboxGL.MapView style={styles.map} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  container: {
+    height: (windowHeight * 0.97),
+    width: (windowWidth * 0.9999)
+  },
+  map: {
+    flex: 1
+  }
+});
 
 export default App;
